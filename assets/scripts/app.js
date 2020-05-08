4cd0a32b94ce44f4bea937d2dcbe9a94462ef376
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-08 06:18:19 +0300

    Controlling Backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,25 @@
 const addMovieModel = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModel.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+	backdrop.classList.toggle('visible');
+};
 
 const toggleMovieModal = () => {
 	addMovieModel.classList.toggle('visible');
+	toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+	toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+	toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
