3346252663b49dc9eb36d93cea0c3daf4c88ea11
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-08 06:33:46 +0300

    Fetching and Controlling User Input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModel = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModel.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModel.querySelectorAll('input');
 
 const toggleBackdrop = () => {
 	backdrop.classList.toggle('visible');
@@ -16,10 +18,27 @@ const backdropClickHandler = () => {
 	toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
 	toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+	const titleValue = userInputs[0].value;
+	const imageUrlValue = userInputs[1].value;
+	const ratingValue = userInputs[2].value;
+
+	if (
+		titleValue.trim() === '' ||
+		imageUrlValue.trim() === '' ||
+		ratingValue.trim() === '' ||
+		+ratingValue < 1 ||
+		+ratingValue > 5
+	) {
+		alert('You should enter a valid input (rating between 1 and 5).');
+	}
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
