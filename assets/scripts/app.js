92b396daba8aa1502d9df2e1b206f60b1b4486de
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-08 07:34:44 +0300

    Rendering Movie Items on the Screen

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,35 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModel.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModel.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () => {
+	if (movies.length === 0) {
+		entryTextSection.style.display = 'display';
+	} else {
+		entryTextSection.style.display = 'none';
+	}
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+	const newMovieElement = document.createElement('li');
+	newMovieElement.className = 'movie-element';
+
+	newMovieElement.innerHTML = `
+        <div class="movie-element__image">
+            <img src="${imageUrl}" alt="${title}" />
+        </div>
+        <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating}/5 rating</p>
+        </div>
+    `;
+
+	const listRoot = document.getElementById('movie-list');
+	listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
 	backdrop.classList.toggle('visible');
@@ -57,6 +84,8 @@ const addMovieHandler = () => {
 
 	toggleMovieModal();
 	clearMovieInput();
+	renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+	updateUI();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
