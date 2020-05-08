676022120ef04790f333c8fd07e07b4ae8b8ba75
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-08 06:05:56 +0300

    Opening Modal by Changing CSS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,8 @@
 const addMovieModel = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+
+const toggleMovieModal = () => {
+	addMovieModel.classList.toggle('visible');
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
